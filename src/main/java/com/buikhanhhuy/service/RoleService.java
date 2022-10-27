563dc5b2a264957c4e6f71a6714477444b72881c
Ngan2001 <71238538+Ngan2001@users.noreply.github.com>
2022-10-27 13:42:00 +0700

    p1

---
+++ src/main/java/com/buikhanhhuy/service/RoleService.java
@@ -0,0 +1,11 @@
+package com.buikhanhhuy.service;
+
+import com.buikhanhhuy.pojo.Role;
+
+import java.util.List;
+
+public interface RoleService {
+    public List<Role> getRoles(String kw);
+    public List<Object[]> getRoleOptions(String[] exceptRole);
+    public Role getRoleByRoleName(String roleName);
+}
